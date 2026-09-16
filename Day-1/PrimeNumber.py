c019fa24c6626c5f9e1961fc6239fa71ec5cc3f4
sahil9464-Hub <sharmasahil1806@gmail.com>
2026-09-16 14:20:43 +0530

    Uncomment prime number checking logic

--- Day-1/PrimeNumber.py
+++ Day-1/PrimeNumber.py
@@ -1,21 +1,21 @@
 # Prime Number
 
  
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-# if num <= 1:
-#     print("Not a Prime Number")
-# else:
-#     i = 2
-#     prime = True
+if num <= 1:
+    print("Not a Prime Number")
+else:
+    i = 2
+    prime = True
 
-#     while i < num:
-#         if num % i == 0:
-#             prime = False
-#             break
-#         i += 1
+    while i < num:
+        if num % i == 0:
+            prime = False
+            break
+        i += 1
 
-#     if prime:
-#         print("Prime Number")
-#     else:
-#         print("Not a Prime Number")
+    if prime:
+        print("Prime Number")
+    else:
+        print("Not a Prime Number")
